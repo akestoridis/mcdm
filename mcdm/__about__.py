3cd7358129e07d7421b2c11448769a8391b68c9b
Dimitrios-Georgios Akestoridis <akestoridis@gmail.com>
2020-01-21 21:09:40 -0800

    Add metadata for the Python package

---
+++ mcdm/__about__.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2020 Dimitrios-Georgios Akestoridis
+# This project is licensed under the terms of the MIT license.
+
+__title__ = "mcdm"
+__version__ = "0.0.dev1"
+__author__ = "Dimitrios-Georgios Akestoridis"
+__author_email__ = "akestoridis@gmail.com"
+__description__ = ("Python implementation of Multiple-Criteria "
+                   "Decision-Making algorithms")
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2020 Dimitrios-Georgios Akestoridis"
+__url__ = "https://github.com/akestoridis/mcdm"
+__keywords__ = ("multiple-criteria decision-making, evaluation, ranking, "
+                "scoring, weighting, correlation, normalization")
+__classifiers__ = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Intended Audience :: Science/Research"
+]
+__requires__ = []
+__python_requires__ = ">=3.5.3"
+__all__ = [
+    "__title__",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__description__",
+    "__license__",
+    "__copyright__",
+    "__url__",
+    "__keywords__",
+    "__classifiers__",
+    "__requires__",
+    "__python_requires__"
+]
---
+++ mcdm/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2020 Dimitrios-Georgios Akestoridis
+# This project is licensed under the terms of the MIT license.
+
+"""
+Python implementation of Multiple-Criteria Decision-Making algorithms
+"""
+
+from .__about__ import *
